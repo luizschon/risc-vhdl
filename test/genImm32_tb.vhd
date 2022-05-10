93d1ce74530293c572c83b9632c17436715702e7
Luiz Schonarth <luizcschonarth@gmail.com>
2022-05-10 16:09:33 -0300

    Update copyright notice

--- test/genImm32_tb.vhd
+++ test/genImm32_tb.vhd
@@ -1,5 +1,23 @@
 -- Artefatos codificados por Luiz Carlos Schonarth Junior, 
 -- matrícula 19/0055171, UnB - Universidade de Brasília
+
+-- Copyright 2022 Luiz Schonarth
+
+-- This file is part of RISC-VHDL.
+
+-- RISC-VHDL is free software: you can redistribute it and/or modify it
+-- under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+
+-- RISC_VHDL  is distributed in the hope that it will be useful, but 
+-- WITHOUT ANY WARRANTY; without even the implied warranty of 
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+-- See the GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License
+-- along with RISC-VHDL. If not, see <https://www.gnu.org/licenses/>.
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
